018f7280f2325ae8794cda91760f4ae73f1c877a
Aniket <aniketdhokane2071@gmail.com>
2021-11-24 16:15:00 +0530

    2nd commit 1:10

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+
+const name = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavour');
+require('./7-mind-grenade')
+
+sayHi('Aniket')
+sayHi('Devesh')
+sayHi('Harshit')
--- app.js
+++ app.js
@@ -1,8 +1,45 @@
+const path = require('path');
+
+console.log(path.sep);
+
+const filePath = path.join('/content/','subholder','test.txt')
+console.log(filePath);
+
+const base=path.basename(filePath)
+console.log(base);
+
+const absolute =path.resolve(__dirname, 'content', 'subfolder','test.txt')
+console.log(absolute);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Modules
-const name = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavour');
-require('./7-mind-grenade')
+// const name = require('./4-names');
+// const sayHi = require('./5-utils');
+// const data = require('./6-alternative-flavour');
+// require('./7-mind-grenade')
 
 
 
